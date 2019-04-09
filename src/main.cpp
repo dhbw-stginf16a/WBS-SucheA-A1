7bd90994cb76cfd555f82ac4508e5922fce8d467
guserav <erik.zeiske@web.de>
2019-04-09 19:34:52 +0200

    Swap input parameter to heightxwidth

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 
 int main(int argc, char *argv[]) {
     if(argc < 6) throw std::runtime_error("Need at least three arguments width, height, dataFile, ArtifactFile, startX, startY");
-    int length = std::stoul(argv[1], nullptr, 10);
-    int height = std::stoul(argv[2], nullptr, 10);
+    int length = std::stoul(argv[2], nullptr, 10);
+    int height = std::stoul(argv[1], nullptr, 10);
     int x = std::stoul(argv[5], nullptr, 10);
     int y = std::stoul(argv[6], nullptr, 10);
 
